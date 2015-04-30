fd36dd1b04508249a78afa0fd7e61b4d92df4459
clareotoole <cotoole06@qub.ac.uk>
2015-04-30 16:00:37 +0100

    update to doctor page

--- PAS_Fx/GUI/application/DoctorPageControl.java
+++ PAS_Fx/GUI/application/DoctorPageControl.java
@@ -2,13 +2,19 @@ package application;
 
 import java.io.IOException;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class DoctorPageControl {
 
@@ -178,7 +184,97 @@ public class DoctorPageControl {
 	    }
 
 	 
+	    private void refreshTable() {
+			if (!GUIMain.patientQueue.isEmpty()) {
+				ObservableList<Patient> list = FXCollections.observableArrayList();
+				nhs_number
+						.setCellValueFactory(new PropertyValueFactory<Patient, Integer>(
+								"nhsNumber"));
+				title.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+						"title"));
+				first_name
+						.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+								"firstName"));
+				last_name
+						.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+								"lastName"));
+				address.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+						"address"));
+				telephone
+						.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+								"contactNum"));
+				allergies
+						.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+								"allergies"));
+				blood_group
+						.setCellValueFactory(new PropertyValueFactory<Patient, String>(
+								"bloodGroup"));
+				triage.setCellValueFactory(new PropertyValueFactory<Patient, Integer>(
+						"triage"));
+				triage.setCellFactory(new Callback<TableColumn<Patient, Integer>, TableCell<Patient, Integer>>() {
 
+					@Override
+					public TableCell<Patient, Integer> call(
+							TableColumn<Patient, Integer> param) {
+
+						return new TableCell<Patient, Integer>() {
+							@Override
+							protected void updateItem(Integer item, boolean empty) {
+								super.updateItem(item, empty);
+								
+								if (item!=null) {
+									for(int loop=0;loop<Triage.values().length;loop++){
+									if(item==Triage.values()[loop].getLevel()){
+										setText(Triage.values()[loop].getTriage());
+									}
+									}
+								}
+							}
+						};
+					}
+				});
+
+				waitingTime.setCellValueFactory(new PropertyValueFactory<Patient,Long>("waitingTime"));
+				waitingTime
+						.setCellFactory(new Callback<TableColumn<Patient, Long>, TableCell<Patient, Long>>() {
+
+							@Override
+							public TableCell<Patient, Long> call(
+									TableColumn<Patient, Long> param) {
+
+								return new TableCell<Patient, Long>() {
+									@Override
+									protected void updateItem(Long item,
+											boolean empty) {
+										super.updateItem(item, empty);
+										if (item != null) {
+											long seconds = item / 1000;
+											if (seconds < 60) {
+												setText("00:"
+														+ String.format("%02d",
+																seconds));
+											} else if (seconds >= 60
+													&& seconds < 3600) {
+												setText(String.format("%02d:",
+														seconds / 60)
+														+ String.format("%02d",
+																seconds % 60));
+											} else {
+												setText("More than one hour");
+											}
+										}
+									}
+								};
+							}
+						});
+				for (Patient patient : GUIMain.patientQueue) {
+					list.add(patient);
+					tableView.setItems(list);
+				}
+			} else {
+				tableView.setItems(null);
+			}
+		}
 	
 	
 }
