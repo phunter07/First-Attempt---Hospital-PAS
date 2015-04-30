677604002172fc879e26f9763530cc766d51e3ac
clareotoole <cotoole06@qub.ac.uk>
2015-04-30 18:08:51 +0100

    Doctor  page updates

--- PAS_Fx/GUI/application/DoctorPageControl.java
+++ PAS_Fx/GUI/application/DoctorPageControl.java
@@ -1,23 +1,62 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
-public class DoctorPageControl {
+public class DoctorPageControl implements Initializable {
 
+    @FXML
+    private TableColumn<Patient, String> allergies;
+
+    @FXML
+    private TableColumn<Patient, String> address;
+
+    @FXML
+    private TableColumn<Patient, Integer> nhs_number;
+
+    @FXML
+    private TableView<Patient> tableView;
+
+    @FXML
+    private TableColumn<Patient, String> last_name;
+
+    @FXML
+    private TableColumn<Patient, Long> waitingTime;
+
+    @FXML
+    private TableColumn<Patient, String> telephone;
+
+    @FXML
+    private TableColumn<Patient, String> title;
+   
+    @FXML
+    private TableColumn<Patient, String> blood_group;
+
+    @FXML
+    private TableColumn<Patient, String> first_name;
+
+    @FXML
+    private TableColumn<Patient, Integer> triage;
+
+	
 	  @FXML
 	    private Button treatmentRoom1;
 
@@ -184,6 +223,49 @@ public class DoctorPageControl {
 	    }
 
 	 
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			Thread myThread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							Thread.sleep(Limits.REFRESHTIME);
+							Platform.runLater(new Runnable() {
+
+								@Override
+								public void run() {
+									refresh();
+
+								}
+							});
+
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			});
+			myThread.setDaemon(true);
+			myThread.start();
+			
+		}
+
+		/**
+		 * the method is used to refresh table, update waiting time of every patient
+		 * in queue and show the Hospital Manager the next patient and the available
+		 * treatment room
+		 * 
+		 * @author
+		 */
+		private void refresh() {
+			refreshTable();
+
+		}
+	    
+	    
 	    private void refreshTable() {
 			if (!GUIMain.patientQueue.isEmpty()) {
 				ObservableList<Patient> list = FXCollections.observableArrayList();
