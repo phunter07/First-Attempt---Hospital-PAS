a32a0fae751ec49b733c208d7ac4de522353e9d8
clareotoole <cotoole06@qub.ac.uk>
2015-04-30 22:22:52 +0100

    update to search patient control

--- PAS_Fx/GUI/receptionistPage/SearchPatientControl.java
+++ PAS_Fx/GUI/receptionistPage/SearchPatientControl.java
@@ -6,6 +6,7 @@
 
 package receptionistPage;
 
+import application.GUIMain;
 import application.Patient;
 import application.Person;
 import javafx.collections.FXCollections;
@@ -51,27 +52,10 @@ public class SearchPatientControl {
 	 * Just add some sample data in the constructor.
 	 */
 	public SearchPatientControl() {
-		masterData.add(new PatientForSearch("1000","Neville","Vance","5 Ballymiscaw Road","9733492995","A"));
-		masterData.add(new PatientForSearch("1001","Patricia","Vance","32 Begny Road","9863527465","AB"));
-		masterData.add(new PatientForSearch("1002","Jennifer","Lyttle","21 Lenaghan Crescent","9863549264","B"));
-		masterData.add(new PatientForSearch("1003","Declan","Cranberry","10 Country Road","9788853741","A"));
-		masterData.add(new PatientForSearch("1004","Bryan","Quinn","12 Farmville Road","9677728491","AB"));
-		masterData.add(new PatientForSearch("1005","Amanda","Tom","21 Glenwood","9866743952","B"));
-		masterData.add(new PatientForSearch("1006","Chris","Tom","8 Shipquay Street","9888885326","AB"));
-		masterData.add(new PatientForSearch("1007","James","Symth","11 Cavehill road","9733492995","B"));
-		masterData.add(new PatientForSearch("1008","Stacey","Monro","21 Johnson drive","9863789891","A"));
-		masterData.add(new PatientForSearch("1009","Andrew","Cusack","54 Ulsterville Gardens","9877738369","O"));
-		masterData.add(new PatientForSearch("1010","Fiona","Cusack","19 Donnybrook Street","9863584765","A"));
-		masterData.add(new PatientForSearch("1011","Claire","Hunter","2 Strand road","9872547665","AB"));
-		masterData.add(new PatientForSearch("1012","Maria","Stephenson","8 Glenavy road","9864763524","AB"));
-		masterData.add(new PatientForSearch("1013","Andrew","Simpson","20 Stormount park","9899763574","B"));
-		masterData.add(new PatientForSearch("1014","Tony","Andrews","14 Spencer road","9765112345","A"));
-		masterData.add(new PatientForSearch("1015","Rachel","Stevens","30 Shankhill road","9833274658","O"));
-		masterData.add(new PatientForSearch("1016","Catherine","Stevens","29 Green grove","9855524356","AB"));
-		masterData.add(new PatientForSearch("1017","Timothy","Stevens","73 Meadow lane","9844499998","O"));
-		masterData.add(new PatientForSearch("1018","Allan","Curry","46 Castle muse","9755375869","B"));
-		masterData.add(new PatientForSearch("1019","Jordan","Watson","51 Grey crescent","9863977648","A"));
-		masterData.add(new PatientForSearch("1020","Agnes","Brown","41 Windview drive","9863587654","O"));
+		for(Patient patient:GUIMain.patientQueue){
+			PatientForSearch patientForSearch=new PatientForSearch(patient.getNhsNumber(), patient.getFirstName(), patient.getLastName(), patient.getAddress(), patient.getContactNumber(), patient.getBloodGroup());
+			
+		}
 	}
 
 	/**
