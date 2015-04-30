de82ab4ecca1c3498011dc68c4786bdd68e42bd6
clareotoole <cotoole06@qub.ac.uk>
2015-04-30 13:05:32 +0100

    update treatments Rooms

--- PAS_Fx/GUI/application/DoctorPageControl.java
+++ PAS_Fx/GUI/application/DoctorPageControl.java
@@ -22,14 +22,19 @@ public class DoctorPageControl {
 	    private Button treatmentRoom5;
 
 	    @FXML
-	    private Button logOut;
-
-	    @FXML
 	    private Button treatmentRoom3;
 
 	    @FXML
 	    private Button treatmentRoom4;
+	    
+	    @FXML
+	    private Button logOut;
 
+	    @FXML
+	    void OnClickLogOut(ActionEvent event) {
+	    
+	    }
+	    
 	    @FXML
 	    void OnClickTreatmentRoom1(ActionEvent event) {
 	    	Stage newStage = new Stage();
@@ -52,22 +57,82 @@ public class DoctorPageControl {
 
 	    @FXML
 	    void onClickTreatmentRoom2(ActionEvent event) {
+	    	Stage newStage = new Stage();
 
+				Parent root;
+				try {
+					root = FXMLLoader.load(getClass().getResource(
+							"/application/TreatmentRoom2.fxml"));
+					Scene scene = new Scene(root, 687, 488);
+					newStage.setTitle("Treatment Room 2");
+					newStage.setScene(scene);
+					newStage.setResizable(false);
+					newStage.show();
+
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
 	    }
 
 	    @FXML
 	    void onClickTreatmentRoom3(ActionEvent event) {
+	    	Stage newStage = new Stage();
 
+				Parent root;
+				try {
+					root = FXMLLoader.load(getClass().getResource(
+							"/application/TreatmentRoom3.fxml"));
+					Scene scene = new Scene(root, 687, 488);
+					newStage.setTitle("Treatment Room 3");
+					newStage.setScene(scene);
+					newStage.setResizable(false);
+					newStage.show();
+
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
 	    }
 
 	    @FXML
 	    void onClickTreatmentRoom4(ActionEvent event) {
+	    	Stage newStage = new Stage();
 
+				Parent root;
+				try {
+					root = FXMLLoader.load(getClass().getResource(
+							"/application/TreatmentRoom4.fxml"));
+					Scene scene = new Scene(root, 687, 488);
+					newStage.setTitle("Treatment Room 4");
+					newStage.setScene(scene);
+					newStage.setResizable(false);
+					newStage.show();
+
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
 	    }
 
 	    @FXML
 	    void onClickTreatmentRoom5(ActionEvent event) {
+	    	Stage newStage = new Stage();
 
+				Parent root;
+				try {
+					root = FXMLLoader.load(getClass().getResource(
+							"/application/TreatmentRoom5.fxml"));
+					Scene scene = new Scene(root, 687, 488);
+					newStage.setTitle("Treatment Room 5");
+					newStage.setScene(scene);
+					newStage.setResizable(false);
+					newStage.show();
+
+				} catch (IOException e) {
+
+					e.printStackTrace();
+				}
 	    }
 
 	 
