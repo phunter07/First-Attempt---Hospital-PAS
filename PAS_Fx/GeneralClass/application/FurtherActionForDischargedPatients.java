ffd1705012aa3ab1a7566c06ad1703c7c9af7ad2
hannahmcdade <hmcdade01@qub.ac.uk>
2015-04-30 15:20:44 +0100

    additions

---
+++ PAS_Fx/GeneralClass/application/FurtherActionForDischargedPatients.java
@@ -0,0 +1,34 @@
+package application;
+
+/**
+ * ENums to hold the further action for patients who have been discharged from A
+ * and E - to be set by the Doctor. A reflection of how the system could be
+ * extended. These will be written to file and stored in the patients records
+ *
+ */
+
+public enum FurtherActionForDischargedPatients {
+
+	DECEASED("Patient has passed away"), PSYCHIATRIST(
+			"This patient has been referred to the psych ward"), WARDS(
+			"Patient recovering in wards"), THEATRE(
+			"Patient transferred to theatre");
+
+	
+	private String furtherAction;
+	
+	
+	private FurtherActionForDischargedPatients(String furtherAction) {
+		this.furtherAction = furtherAction;
+	}
+
+	public String getFurtherAction() {
+		return furtherAction;
+	}
+
+	public void setFurtherAction(String furtherAction) {
+		this.furtherAction = furtherAction;
+	}
+
+}
+
